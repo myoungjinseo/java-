5f0c716b7fe90aec45b1bbd9daf937adeadfd098
myoungjinseo <80959635+myoungjinseo@users.noreply.github.com>
2021-09-17 22:01:08 +0900

    Add files via upload
    자바 1주차

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+
+    public static void main(String[] args) {
+    	System.out.println("202014132");
+    	System.out.println("서명진");
+    	System.out.println("안녕하세요.");
+    }
+
+}
